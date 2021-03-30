e3b82986b9b2c1ef0df6a7e6b84f42b6ecf132b9
KailanySousa <kailany-sousa2010@hotmail.com>
2021-03-30 16:24:00 -0300

    lista para widgets #01

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -37,6 +37,11 @@ class _PerguntaAppState extends State<PerguntaApp> {
       }
     ];
 
+    List<Widget> respostas = [];
+    for (String textoResp in perguntas[_perguntaSelecionada]['respostas']) {
+      respostas.add(Resposta(textoResp, _responder));
+    }
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
@@ -45,9 +50,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
           body: Column(
             children: [
               Questao(perguntas[_perguntaSelecionada]['texto']),
-              Resposta('Resposta 1', _responder),
-              Resposta('Resposta 2', _responder),
-              Resposta('Resposta 3', _responder),
+              ...respostas,
             ],
           )),
     );
